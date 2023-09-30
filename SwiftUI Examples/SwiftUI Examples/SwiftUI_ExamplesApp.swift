a444843283042f281469e0181359e831c4931230
Aynur19 <nasybullin.aynur@yandex.ru>
2023-09-30 19:32:38 +0300

    created new swift ui project;

---
+++ SwiftUI Examples/SwiftUI Examples/SwiftUI_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExamplesApp.swift
+//  SwiftUI Examples
+//
+//  Created by Aynur Nasybullin on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
